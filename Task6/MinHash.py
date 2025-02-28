3c80b6e0cf9dfb3ab06325dc27dbba29aea8eaa4
smnk-1 <semikinnikolay1@gmail.com>
2025-02-28 21:10:24 +0600

    feat: MinHash

--- Task6/MinHash.py
+++ Task6/MinHash.py
@@ -1,15 +1,12 @@
+import mmh3
 from random import randint
-
-
-def hash_with_seed(seed):
-    return lambda value: seed*hash(value)
 
 
 def get_seeds(n):
     seeds = set()
     while len(seeds) < n:
-        seeds.add(randint(-9999, 9999))
-    return seeds
+        seeds.add(randint(1, 10000))
+    return list(seeds)
 
 
 def compare(signature1, signature2):
@@ -19,14 +16,11 @@ def compare(signature1, signature2):
 
 class MinHashAlgorithm:
     def __init__(self, k):
-        self.k = k
-        self.hash_funcs = [hash_with_seed(seed) for seed in get_seeds(k)]
+        self.seeds = get_seeds(k)
 
     def execute(self, array):
         signature = []
-        for h in self.hash_funcs:
-            hash_res = []
-            for element in array:
-                hash_res.append(h(element))
-            signature.append(min(hash_res))
+        for seed in self.seeds:
+            hash_values = [mmh3.hash(x, seed=seed) for x in array]
+            signature.append(min(hash_values))
         return signature
--- Task6/MinHashTests.py
+++ Task6/MinHashTests.py
@@ -1,36 +1,52 @@
 from MinHash import MinHashAlgorithm, compare
-from random import randint
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def jaccard_similarity(A, B):
-    a = set(A)
-    b = set(B)
-    union = set.union(a, b)
-    intersection = set.intersection(a, b)
-    if len(union) != 0:
-        return len(intersection)/len(union)
-    else:
-        return 1
+    setA = set(A)
+    setB = set(B)
+    union = setA.union(setB)
+    intersection = setA.intersection(setB)
+    return len(intersection) / len(union) if union else 1
 
-common_elements = [f"word_{i}" for i in range(70)]
 
-# 15 уникальных элементов для A
-unique_A = [f"unique_A_{i}" for i in range(70, 85)]
+common_count = 6500
+unique_count = 1750
 
-# 15 уникальных элементов для B
-unique_B = [f"unique_B_{i}" for i in range(85, 100)]
 
-# Создаём массивы
+common_elements = [f'common_{i}' for i in range(common_count)]
+
+unique_A = [f'unique_A_{i}' for i in range(common_count, common_count + unique_count)]
+unique_B = [f'unique_B_{i}' for i in range(common_count + unique_count, common_count + 2 * unique_count)]
+
 A = common_elements + unique_A
 B = common_elements + unique_B
 
-for k in range(1, 20):
-    j = 0
-    J = jaccard_similarity(A, B)
-    for i in range(0, 10):
+
+J_true = jaccard_similarity(A, B)
+print('Истинная Jaccard-схожесть:', J_true)
+
+
+results = []
+trials = 15
+for k in range(1, 25, 2):
+    total = 0
+    for _ in range(trials):
         mnh = MinHashAlgorithm(k)
         s1 = mnh.execute(A)
         s2 = mnh.execute(B)
-        j += compare(s1, s2)
-    print(J, j/10)
+        total += compare(s1, s2)
+    estimated = total / trials
+    print(f'k = {k}, Jaccard = {J_true:.2f}, MinHash оценка = {estimated:.2f}')
+    results.append({'k': k, 'J_abs_dif': abs(J_true-estimated)*100})
 
+df = pd.DataFrame(results)
+
+plt.plot(df['k'], df['J_abs_dif'])
+plt.xlabel('k')
+plt.ylabel('Абсолютная разница (%)')
+plt.title('Зависимость разницы между J и estimated от k')
+plt.grid(True)
+plt.savefig('../Images/MinHashAbsDiffBetwJAndEstimated.png')
+plt.show()
