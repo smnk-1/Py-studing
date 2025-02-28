8ccc5be49109c8244cad409cd7dcd63a4b767db4
smnk-1 <semikinnikolay1@gmail.com>
2025-02-28 09:36:04 +0600

    feat: update MinHash Algorithm

--- Task6/MinHash.py
+++ Task6/MinHash.py
@@ -1,7 +1,9 @@
 from random import randint
 
+
 def hash_with_seed(seed):
     return lambda value: seed*hash(value)
+
 
 def get_seeds(n):
     seeds = set()
@@ -9,13 +11,22 @@ def get_seeds(n):
         seeds.add(randint(-9999, 9999))
     return seeds
 
+
+def compare(signature1, signature2):
+    matches = sum(1 for x, y in zip(signature1, signature2) if x == y)
+    return matches / len(signature1)
+
+
 class MinHashAlgorithm:
     def __init__(self, k):
         self.k = k
         self.hash_funcs = [hash_with_seed(seed) for seed in get_seeds(k)]
 
     def execute(self, array):
-        return [min([hash_func(element) for element in array]) for hash_func in self.hash_funcs]
-
-    def compare(self, min_hashes1, min_hashes2):
-        pass
+        signature = []
+        for h in self.hash_funcs:
+            hash_res = []
+            for element in array:
+                hash_res.append(h(element))
+            signature.append(min(hash_res))
+        return signature
--- Task6/MinHashTests.py
+++ Task6/MinHashTests.py
@@ -1,6 +1,19 @@
 from MinHash import MinHashAlgorithm
 from random import randint
 
+
+def jaccard_similarity(A, B):
+    a = set(A)
+    b = set(B)
+    union = set.union(a, b)
+    intersection = set.intersection(a, b)
+    if len(union) != 0:
+        return len(intersection)/len(union)
+    else:
+        return 1
+
+
 def generate_items(n):
     return [str(randint(0, 10_000)) for _ in range(0, n)]
+
 
