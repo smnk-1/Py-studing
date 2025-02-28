53e2e4ef608a5376e208b9732d14efa20c99c31e
smnk-mcb <semikinnikolay1@gmail.com>
2025-02-28 20:28:39 +0600

    fix: min hash tests

--- Task6/MinHashTests.py
+++ Task6/MinHashTests.py
@@ -1,4 +1,4 @@
-from MinHash import MinHashAlgorithm
+from MinHash import MinHashAlgorithm, compare
 from random import randint
 
 
@@ -12,8 +12,25 @@ def jaccard_similarity(A, B):
     else:
         return 1
 
+common_elements = [f"word_{i}" for i in range(70)]
 
-def generate_items(n):
-    return [str(randint(0, 10_000)) for _ in range(0, n)]
+# 15 уникальных элементов для A
+unique_A = [f"unique_A_{i}" for i in range(70, 85)]
 
+# 15 уникальных элементов для B
+unique_B = [f"unique_B_{i}" for i in range(85, 100)]
+
+# Создаём массивы
+A = common_elements + unique_A
+B = common_elements + unique_B
+
+for k in range(1, 20):
+    j = 0
+    J = jaccard_similarity(A, B)
+    for i in range(0, 10):
+        mnh = MinHashAlgorithm(k)
+        s1 = mnh.execute(A)
+        s2 = mnh.execute(B)
+        j += compare(s1, s2)
+    print(J, j/10)
 
